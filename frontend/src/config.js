c105d7e36c13b40e7ed5e5d5bd00d309e761fa8f
Pablo Robledo <p.robledo@thermosolutions.cl>
2025-08-06 00:25:52 -0400

    feat: create config paths

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,9 @@
 const API_BASE_URL = process.env.REACT_APP_PATH_TO_API;
+const WS_BASE_URL = process.env.REACT_APP_WS_PATH_TO_API;
 
-export default API_BASE_URL;
+const Path = {
+  API_BASE_URL,
+  WS_BASE_URL,
+};
+
+export default Path;
